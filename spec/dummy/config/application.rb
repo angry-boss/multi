1970667f8024d773f4ac416b80e0842da2ad96b6
Administrator <ilmirashaikhraznova@gmail.com>
2020-02-13 22:11:22 +0100

    delete all adapters without postgresqls adapter

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -15,7 +15,7 @@ module Dummy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     require 'multi/logic/subdomain'
-    require 'multi/logic/domain'
+    # require 'multi/logic/domain'
 
     config.middleware.use Multi::Logic::Subdomain
 
